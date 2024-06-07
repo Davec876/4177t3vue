6ba37f9a53e8db72fa9752f7eca2437f47ebaa6c
Dave Chuck <dv453202@dal.ca>
2024-06-07 14:52:28 -0300

    update for methods

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -15,6 +15,22 @@
       </div>
       <button type="submit" :disabled="!isValid">Submit</button>
     </form>
+    <div>
+      <h2>Examples from Slide Deck</h2>
+      <p>Text Interpolation: {{ greeting }}</p>
+      <p>Computed Property: {{ reversedGreeting }}</p>
+      <p>
+        Conditional Rendering:
+        <span v-if="isValid">Form is valid!</span>
+        <span v-else>Form is invalid!</span>
+      </p>
+      <div>
+        <h3>List Rendering</h3>
+        <ul>
+          <li v-for="item in items" :key="item">{{ item }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,11 +41,16 @@ export default {
     return {
       firstName: "",
       password: "",
+      greeting: "Welcome to Vue.js",
+      items: ["Item 1", "Item 2", "Item 3"],
     };
   },
   computed: {
     isValid() {
       return this.firstName.length > 0 && this.password.length >= 8;
+    },
+    reversedGreeting() {
+      return this.greeting.split("").reverse().join("");
     },
   },
   methods: {
